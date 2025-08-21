e54c620bf60361e93572cedbe2f8085dde5dd584
Pranav <vanarp2205@gmail.com>
2025-08-21 23:34:03 +0800

    Add list feature and task storing

--- src/main/java/BotChat.java
+++ src/main/java/BotChat.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class BotChat {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        String[] tasks = new String[100];
+
 
         String logo = "BotChat";
         String line = "____________________________________________________________";
@@ -13,19 +16,32 @@ public class BotChat {
 
         while (true) {
             String input = scanner.nextLine();
+            //exit feature
             if (input.equals("bye")) {
                 System.out.println(line);
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(line);
                 break;
+
+            } else if (input.equals("list")) {
+                // list feature
+                System.out.println(line);
+                for (int i = 0; i < count; i++) {
+                    System.out.println( (i+1) +". "+ tasks[i]);
+
+                }
+                System.out.println(line);
+
+            } else{
+                // add tasks feature
+                tasks[count] = input;
+                count++;
+                System.out.println(line);
+                System.out.println("added: " + input);
+                System.out.println(line);
             }
 
-            System.out.println(line);
-            System.out.println(input);
-            System.out.println(line);
-
         }
-
 
     }
 }
