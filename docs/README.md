c3a80699986cd0054c6fe82898e2c00b2a6de6ae
Pranav <vanarp2205@gmail.com>
2025-08-21 22:42:13 +0800

    Rename chatbot and implement skeletal behaviour

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# BotChat User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/BotChat.java
@@ -0,0 +1,12 @@
+public class BotChat {
+    public static void main(String[] args) {
+        String logo = "BotChat";
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm " + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("_____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon !");
+        System.out.println("_____________________________________________________________");
+
+    }
+}
