f592b3edf453792519dcc22195ac56cfa5cf111e
Pranav <vanarp2205@gmail.com>
2025-08-21 23:06:27 +0800

    Add echo feature and exit feature with bye command

--- src/main/java/BotChat.java
+++ src/main/java/BotChat.java
@@ -1,12 +1,31 @@
+import java.util.Scanner;
+
 public class BotChat {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String logo = "BotChat";
-        System.out.println("____________________________________________________________");
+        String line = "____________________________________________________________";
+        System.out.println(line);
         System.out.println("Hello! I'm " + logo);
         System.out.println("What can I do for you?");
-        System.out.println("_____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon !");
-        System.out.println("_____________________________________________________________");
+        System.out.println(line);
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(line);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+
+            System.out.println(line);
+            System.out.println(input);
+            System.out.println(line);
+
+        }
+
 
     }
 }
